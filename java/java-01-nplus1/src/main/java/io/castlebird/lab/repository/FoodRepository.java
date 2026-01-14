8e178b2a0d45e878dde003e2b0aeec04445ddfc8
castle-bird <seongjo0501@gmail.com>
2026-01-14 09:28:48 +0900

    feat: batch_fetch_size로 N + 1 해결

--- java/java-01-nplus1/src/main/java/io/castlebird/lab/repository/FoodRepository.java
+++ java/java-01-nplus1/src/main/java/io/castlebird/lab/repository/FoodRepository.java
@@ -1,7 +1,6 @@
 package io.castlebird.lab.repository;
 
 import io.castlebird.lab.entity.Food;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,8 +12,8 @@ public interface FoodRepository extends JpaRepository<Food, Long> {
     @Query("SELECT f FROM Food f JOIN FETCH f.category")
     List<Food> findAllJPQL();
 
-    // @EntityGraph을 테스트할때는 주석 해제
-    @Override
-    @EntityGraph(attributePaths = {"category"})
-    List<Food> findAll();
+    // @EntityGraph을 테스트할때는 주석 해제. 안할땐 주석
+//    @Override
+//    @EntityGraph(attributePaths = {"category"})
+//    List<Food> findAll();
 }
